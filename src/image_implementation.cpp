e10fbf86eb28652b7bc2943987363e39c58219e5
sl4Jd <jovic.andrej5@gmail.com>
2025-08-19 23:32:57 +0200

    added audio

--- src/image_implementation.cpp
+++
@@ -1,2 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include <imgui/stb_image.h>
--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -25,6 +25,51 @@ AppState currentAppState = AppState::MainMenu;
 
 GLFWwindow* main_window;
 
+static ma_decoder decoder; 
+static ma_device device;
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+    if (pDecoder == NULL) {
+        return;
+    }
+
+    // Read PCM frames from the decoder and put them into the output buffer
+    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, NULL);
+
+    (void)pInput; // Prevent unused variable warning
+}
+
+void load_audio(){
+    const char* filename = "src/sound/I_Wonder.mp3"; 
+    if(ma_decoder_init_file(filename, NULL, &decoder) != MA_SUCCESS) {
+        printf("Failed to load audio file: %s\n", filename);
+        return;
+    }
+
+    // Configure playback device
+    ma_device_config deviceConfig = ma_device_config_init(ma_device_type_playback);
+    deviceConfig.playback.format   = decoder.outputFormat;
+    deviceConfig.playback.channels = decoder.outputChannels;
+    deviceConfig.sampleRate        = decoder.outputSampleRate;
+    deviceConfig.dataCallback      = data_callback;
+    deviceConfig.pUserData         = &decoder;
+
+    // Initialize playback device
+    if(ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+        printf("Failed to initialize playback device.\n");
+        ma_decoder_uninit(&decoder);
+        return;
+    }
+
+    // Start playback
+    if (ma_device_start(&device) != MA_SUCCESS) {
+        printf("Failed to start playback device.\n");
+        ma_device_uninit(&device);
+        ma_decoder_uninit(&decoder);
+        return;
+    }
+}
 void ShowStartWindow() {
     ImGui::SetNextWindowSize(ImVec2(windowWidth, windowHeight), ImGuiCond_Always);
     ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
@@ -104,6 +149,8 @@ int main(int, char**)
     ImGui_ImplGlfw_InitForOpenGL(main_window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
+    // Load audio
+    load_audio();
     // Main loop
     while (!glfwWindowShouldClose(main_window))
     {
@@ -144,6 +191,8 @@ int main(int, char**)
         StartNewProject();
     }
     // Cleanup
+    ma_device_uninit(&device);
+    ma_decoder_uninit(&decoder);
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
