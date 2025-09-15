9640c852ecdce657481e45064815a51a8ee81004
sl4Jd <jovic.andrej5@gmail.com>
2025-09-15 22:46:17 +0200

    added conan package manager

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conan import ConanFile
+
+class Conan(ConanFile):
+    name = "Multiscale_sandbox"
+    version = "0.1.0"
+    requires = (
+        "assimp/5.4.3",
+        "glm/1.0.1",
+        "gtk/system",
+        "glfw/3.4"
+    )
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "CMakeDeps", "CMakeToolchain"
