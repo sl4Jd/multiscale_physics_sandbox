82354b3ff806fa73b72f599ec6c156a8dd6aa780
slAJd <jovic.andrej5@gmail.com>
2025-10-26 23:59:29 +0100

    fix opening file bug

--- src/utils/zip_utils.cpp
+++ src/utils/zip_utils.cpp
@@ -3,29 +3,35 @@
 #include <stdexcept>
 #include "zip_utils.h"
 #include <filesystem>
+#include <thread>
 
-using namespace std;
+void CreateProjectZipFile(const std::string& zipName) {
 
-void CreateProjectZipFile(const string& zipName) {
-
-    string scene = "{\n  \"name\": \"" + zipName + "\"\n}";
+    std::string scene = "{\n  \"name\": \"" + zipName + "\"\n}";
 
     mz_zip_archive zipArchive;
     memset(&zipArchive, 0, sizeof(zipArchive));
 
     if (!mz_zip_writer_init_file(&zipArchive, ("user_data/projects/" + zipName + ".msps").c_str(), 0)) {
-        throw runtime_error("Failed to initialize zip writer");
+        throw std::runtime_error("Failed to initialize zip writer");
     }
 
-    if (!mz_zip_writer_add_file(&zipArchive, "scene.json","user_data/projects/working/scene.json", nullptr, 0, 0)) {
+    if (!mz_zip_writer_add_file(
+        &zipArchive,
+        "scene.json",
+        "user_data/projects/working/scene.json",
+        NULL,
+        0,
+        MZ_NO_COMPRESSION))
+    {
         mz_zip_writer_end(&zipArchive);
-        throw runtime_error("Failed to add scene.json to zip");
+        throw std::runtime_error("Failed to add scene.json to zip");
     }
-
     if (!mz_zip_writer_finalize_archive(&zipArchive)) {
         mz_zip_writer_end(&zipArchive);
-        throw runtime_error("Failed to finalize zip archive");
+        throw std::runtime_error("Failed to finalize zip archive");
     }
+    
 
     mz_zip_writer_end(&zipArchive);
 }
@@ -33,7 +39,7 @@ void UnzipFile(const std::string& zipName) {
     mz_zip_archive zipArchive;
     memset(&zipArchive, 0,sizeof(zipArchive));
     if (!mz_zip_reader_init_file(&zipArchive, ("user_data/projects/" + zipName + ".msps").c_str(), 0)) {
-        throw runtime_error("Failed to open zip file");
+        throw std::runtime_error("Failed to open zip file");
     }
     int file_count = (int)mz_zip_reader_get_num_files(&zipArchive);
 
@@ -42,7 +48,7 @@ void UnzipFile(const std::string& zipName) {
         mz_zip_archive_file_stat file_stat;
         if (!mz_zip_reader_file_stat(&zipArchive, i, &file_stat)) {
             mz_zip_reader_end(&zipArchive);
-            throw runtime_error("Failed to get file stat for index" + to_string(i));
+            throw std::runtime_error("Failed to get file stat for index" + std::to_string(i));
         }
 
         // Skip directories
@@ -51,18 +57,21 @@ void UnzipFile(const std::string& zipName) {
         }
 
         // Build full output path
-        string output_dir = "user_data/projects/working";
-        string out_path = output_dir + "/" + file_stat.m_filename;
+        std::string output_dir = "user_data/projects/working";
+        std::string out_path = output_dir + "/" + file_stat.m_filename;
 
         //make subdirs
-        filesystem::create_directories(std::filesystem::path(out_path).parent_path());
+        std::filesystem::create_directories(std::filesystem::path(out_path).parent_path());
         // Extract the file
         if (!mz_zip_reader_extract_to_file(&zipArchive, i, out_path.c_str(), 0)) {
             mz_zip_reader_end(&zipArchive);
-            throw runtime_error("Failed to extract file " + string(file_stat.m_filename));
+            throw std::runtime_error("Failed to extract file " + std::string(file_stat.m_filename));
         }
-    }
 
+    }
+    
     // Close the archive
     mz_zip_reader_end(&zipArchive);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
